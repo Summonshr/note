dacadb15999555424254a00205282e4a412512a9
suman shretha <root@laravelnepal.com>
2018-03-02 06:08:50 +0000

    aded

--- routes/web.php
+++ routes/web.php
@@ -13,15 +13,29 @@
 Route::get('/', function(){
     return redirect(str_random(10));
 });
+
 Route::get('{name}/html', function(){
 	return cache()->get(request()->route('name'),'Nothing found');
 });
+
 Route::get('{name}/image', function(){
         return SnappyImage::loadFile(url(request()->route('name')).'/html')->download(request()->route('name').'.jpg');
 });
+
+Route::get('{name}/mail', function(){
+	rescue(function(){
+Mail::send('mail', ['content'=>cache()->get(request()->route('name'))], function($m){
+        $m->from('noreply@pdfpub.com','PDFPUB.com');
+        $m->to(request()->get('mail'))->subject('Note about '.request()->route('name'));
+        });
+});	
+	return back();
+});
+
 Route::get('{name}/pdf', function(){
 	return PDF::loadFile(url(request()->route('name')).'/html')->download(request()->route('name').'.pdf');
 });
+
 Route::get('{name}', function () {
     return view('welcome');
 });
