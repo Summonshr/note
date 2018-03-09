3a046e2e5e0d6a8fea1dac0a444eb587f0322949
Suman Shrestha <summonshr@gmail.com>
2018-03-09 17:58:11 +0545

    done

--- app/Map.php
+++ app/Map.php
@@ -5,44 +5,35 @@ class Map
     public $attributes = [
         'center'=>[],
         'markers'=>[],
-        'zoom'=>4
+        'zoom'=>4,
+        'routes'=>[]
     ];
-
     public function __construct($lat, $lng)
     {
         $this->attributes['center']['lat'] = $lat;
         $this->attributes['center']['lng'] = $lng;
     }
-
     public function __toString(){
         return $this->render();
     }
-
     public function pushAttributes($attribute, $value){
         $this->attributes[$attribute][] = $value;
         return $this; 
     }
-
     public function mergeAttributes($attribute, $value){
-
         $this->attributes[$attribute] = array_merge($this->attributes[$attribute], $value);
         return $this;
     }
-
     public function getAttributes(){
         return $this->attributes;
     }
-
     public function zoom($level = 4){
         $this->attributes['zoom'] = $level;
         return $this;
     }
-
     public function render(){
         return '<div class="map" style="width: 80%; height: 300px; background-color: grey;" data-attributes=\''.json_encode($this->getAttributes()).'\'></div>';
     }
-
-
     // Markers
     public function addMarker($lat, $lng, $content = null){
         $this->pushAttributes('markers',['lat'=>$lat,'lng'=>$lng,'content'=>$content ?:['html'=>'marker content missing','title'=>'title missing']]);
@@ -56,16 +47,29 @@ class Map
         return $this;
     }
     // Clusters
-    public function addCluster($key, $markers){
-        $this->attributes['clusters'][$key][] = $markers;
+    public function addCluster($markers){
+        $this->attributes['clusters'][] = $markers;
         return $this;
     }
 
     public function addClusters(array $clusters){
         foreach($clusters as $key => $cluster){
-            $this->attributes['clusters'][$cluster['key'] ?? $key][] = $markers;
+            $this->attributes['clusters'][] = $cluster;
         }
         return $this;
     }
-    //
+    public function drawLine(array $line){
+        $this->attributes['lines'][] = array_map(function($val){
+            return array_combine(array_slice(['lat','lng','content'],0,count($val)),$val);
+        },array_filter($line, 'is_array'));
+        return $this;
+    }
+    public function drawLines(array $lines){
+        foreach($lines as $line){
+            $this->attributes['lines'][] = array_map(function($val){
+                return array_combine(array_slice(['lat','lng','content'],0,count($val)),$val);
+            },array_filter($line, 'is_array'));
+        }
+        return $this;
+    }
 }
--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -6,21 +6,49 @@
         </style>
         </head>
         <body>
-            {!! map(-25.363, 131.044)->zoom(8)->addMarker(-24.363,131.044,['html'=>'somerthing','title'=>'something']) !!}
-        </body>       <script>
+            {!! map(-25.363, 131.044)->zoom(8)->drawLines(
+                    [
+                        [
+                            'locations'=>[
+                                [-12.044012922866312, -77.02470665341184],
+                                [-12.05449279282314, -77.03024273281858],
+                                [-12.055122327623378, -77.03039293652341],
+                                [-12.075917129727586, -77.02764635449216],
+                                [-12.07635776902266, -77.02792530422971],
+                                [-12.076819390363665, -77.02893381481931],
+                                [-12.088527520066453, -77.0241058385925],
+                                [-12.090814532191756, -77.02271108990476]
+                            ]
+                        ],
+                        [
+                            'locations'=>[
+                                [-12.044012922866312, -77.02470665341184],
+                                [-12.05449279282314, -77.03024273281858],
+                                [-12.055122327623378, -77.03039293652341],
+                                [-12.075917129727586, -77.02764635449216],
+                                [-12.07635776902266, -77.02792530422971],
+                                [-12.076819390363665, -77.02893381481931],
+                                [-12.088527520066453, -77.0241058385925],
+                                [-12.090814532191756, -77.02271108990476]
+                            ]
+                        ]
+                    ]
+                 ) !!}
+        </body>       
+        <script>
             function map(elem){
                 if(elem.dataset && ( data = JSON.parse(elem.dataset.attributes))){
-                   
                     var map = new google.maps.Map(elem, {
                         zoom: data.zoom || 4,
                         center: data.center || {lat: -25.363, lng: 131.044}
                     });
 
-                    data.markers && data.markers.length > 0 && data.markers.map(function(marker){
+                    var pinMarker = function (marker, index){
                         var pin = new google.maps.Marker({
                             position: marker,
                             map: map,
-                            title: marker.content && marker.content.title
+                            label: String((marker.content && marker.content.label) || (index + 1)),
+                            title: String((marker.content && marker.content.title) || (index + 1))
                         });
                         if(marker.content){
                             var infowindow = new google.maps.InfoWindow({
@@ -31,11 +59,22 @@
                                 infowindow.open(map, pin);
                             });
                         }
-                    });
+                        return pin;
+                    }
 
-                    data.clusters && data.clusters.length > 0 && data.clusters.map(function(cluster){
-                        console.log(cluster);
-                    });
+                    var pinLine = function(line, index){
+                        var flightPath = new google.maps.Polyline(line);
+
+                        flightPath.setMap(map);
+                    }
+
+                    var pinCluster = function(cluster){
+                        return cluster.locations && cluster.locations.length > 0 && new MarkerClusterer(map, cluster.locations.map(pinMarker), {imagePath: cluster.image || 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+                    }
+
+                    data.markers && data.markers.length > 0 && data.markers.map(pinMarker);
+                    data.clusters && data.clusters.length > 0 && data.clusters.map(pinCluster);
+                    data.lines && data.lines.length > 0 && data.lines.map(pinLine);
                 }
             }
             function initMap(){
@@ -45,5 +84,6 @@
                 }
             }
         </script>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/js-marker-clusterer/1.0.0/markerclusterer.js"></script>
     <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAQ_xaCYxLHL_ReW4DqxQK74sVZ7UoQR1Y&callback=initMap"></script>
 </html>
